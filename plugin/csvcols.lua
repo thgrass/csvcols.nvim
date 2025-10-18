67915029f6737826d578014801ec793bf2620abe
thrass <tg@grassmann-code.de>
2025-10-18 16:57:50 +0200

    Added defaults for sticky header to init.lua

--- plugin/csvcols.lua
+++ plugin/csvcols.lua
@@ -2,6 +2,9 @@
 if vim.g.loaded_csvcols then return end
 vim.g.loaded_csvcols = true
 pcall(function()
-  require("csvcols").setup()  -- users can still override by calling setup() later
+  require("csvcols").setup({
+	  default_header_lines = 1,     -- lines in sticky header
+	  use_winbar_controls = true,   -- show clickable buttons (only when mouse supported)
+  })  -- users can still override by calling setup() later
 end)
 
